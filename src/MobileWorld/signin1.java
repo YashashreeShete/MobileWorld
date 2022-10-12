35cd1af75707ab40a595aece184a025798c645be
YashashreeShete <113904382+YashashreeShete@users.noreply.github.com>
2022-10-12 14:33:23 +0530

    Update signin1.java

--- src/MobileWorld/signin1.java
+++ src/MobileWorld/signin1.java
@@ -1,29 +1,45 @@
 package MobileWorld;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 public class signin1 {
-@Test
+@Test(dataProvider = "getData")
 	
-		public static void signin() throws InterruptedException {
+		public static void signin(String username, String password12) throws InterruptedException {
 		
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\yashashree.shete\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
+	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\yashashree.shete\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+      
+        driver.get("https://qualicoach.org/mwapp/index.html");
     
-    
-    driver.get("https://mobileworld.banyanpro.com/");
-    
-   LandingPage landingpage = new LandingPage(driver);
+        LandingPage landingpage = new LandingPage(driver);
     
    
-   Thread.sleep(1000);
-   landingpage.goToSignin(); 
-   landingpage.signin("yashashree988@gmail.com", "shree24");
-   Thread.sleep(1000);
-    landingpage.goToLogin(); 
+        Thread.sleep(1000);
+        landingpage.goToSignin(); 
+        // landingpage.signin("yashashree988@gmail.com", "shree24");
+        landingpage.signin(username,password12);
+        Thread.sleep(1000);
+        landingpage.goToLogin(); 
    
 	}
+@DataProvider
+public Object[][] getData(){
+     Object[][] data = new Object[3][2];
+     
+     data[0][0]="yashashree67";
+     data[0][1]="shree826";
+     
+     data[1][0]="8757657623";
+     data[1][1]="@#$$%%";
+     
+     data[2][0]="";
+     data[2][1]="";
+     return data;
+}
+
 }
